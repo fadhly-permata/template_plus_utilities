032bda65416ecd979f83ebf13d615f321dffbe53
fadhly.permata <fadhly.permata@idxpartners.com>
2025-03-27 09:37:19 +0700

    checkpoint

--- IDC.Template/Controllers/MongoController.TTable.cs
+++ IDC.Template/Controllers/MongoController.TTable.cs
@@ -35,35 +35,3 @@ public class TTableController(
     Language language,
     SystemLogging systemLogging
 ) : MongoController(mongoHelper, language, systemLogging, "TTable") { }
-
-/// <summary>
-/// Controller for managing temporary tables in MongoDB
-/// </summary>
-/// <remarks>
-/// Provides CRUD operations for temporary table management with integrated logging and error handling.
-/// Inherits base functionality from <see cref="MongoController"/>.
-///
-/// Example usage:
-/// <code>
-/// POST api/TTable
-/// {
-///   "name": "tempTable1",
-///   "data": [
-///     { "id": 1, "value": "test" }
-///   ]
-/// }
-/// </code>
-/// </remarks>
-/// <param name="mongoHelper">MongoDB helper instance for database operations</param>
-/// <param name="language">Language service for localized messages</param>
-/// <param name="systemLogging">Logging service for system operations</param>
-/// <seealso cref="MongoController"/>
-/// <seealso cref="MongoHelper"/>
-[Route("api/[controller]")]
-[Tags(tags: "Users Managements")]
-[ApiExplorerSettings(IgnoreApi = false)]
-public class UsersController(
-    MongoHelper mongoHelper,
-    Language language,
-    SystemLogging systemLogging
-) : MongoController(mongoHelper, language, systemLogging, "Users") { }
